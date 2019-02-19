fdb79005e48468af5b414b5566ee5ee22455c358
Anton Denisenko <badbondmimimirusc@yandex.ru>
2019-02-19 15:26:52 +0300

    Update config.js

--- examples/basics/config.js
+++ examples/basics/config.js
@@ -1,4 +1,6 @@
-System.config({
+System.config
+(
+  {
   baseURL: "/",
   defaultJSExtensions: true,
   transpiler: "babel",
@@ -8,6 +10,7 @@ System.config({
       "optimisation.modules.system"
     ]
   },
+  
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
@@ -21,40 +24,51 @@ System.config({
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
+    
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
     },
+    
     "github:jspm/nodelibs-process@0.1.2": {
       "process": "npm:process@0.11.2"
     },
+    
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
+    
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
+    
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
+    
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.1"
     },
+    
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    
     "npm:lucid-router@1.3.5": {
       "process": "github:jspm/nodelibs-process@0.1.2",
       "url-pattern": "npm:url-pattern@1.0.1"
     },
+    
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
+    
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
+    
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
